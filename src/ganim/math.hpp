efab6b4ae2f85450a82acccce0ba76cb77f44f89
David Cohoe <sudgylacmoe@gmail.com>
2023-11-12 21:24:30 -0800

    Added the most important mathematical constant

---
+++ src/ganim/math.hpp
@@ -0,0 +1,16 @@
+#ifndef GANIM_MATH_HPP
+#define GANIM_MATH_HPP
+
+/** @file
+ * @brief Miscellaneous mathematical utitilies
+ *
+ * If there's anything mathematical that doesn't really fit in a different part
+ * of the library, it will be here.
+ */
+
+/** @brief The number of radians in one turn of a circle */
+inline constexpr double tau = 6.28318530717958647692;
+/** @brief An alias for @ref tau if you want to be fancy */
+inline constexpr double τ = tau;
+
+#endif
