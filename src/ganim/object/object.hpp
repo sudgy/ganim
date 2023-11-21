93086d9bbcf545cdf8f912d6fbea7b77455905f4
David Cohoe <sudgylacmoe@gmail.com>
2023-11-20 16:02:38 -0800

    Added a bit of missing documentation

--- src/ganim/object/object.hpp
+++ src/ganim/object/object.hpp
@@ -11,12 +11,27 @@
 
 namespace ganim {
     /** @brief The base class for objects that have some kind of extent
+     *
+     * Most of the functions in this class return `*this` to allow for chaining.
+     * If you want the chaining to work on subclasses, use the @ref
+     * GANIM_OBJECT_CHAIN_DECLS macro.
      */
     class Object : public Transformable {
         public:
+            /** @brief Set the color of this object, ignoring alpha. */
             Object& set_color(Color color);
+            /** @brief Set the color of this object, including alpha. */
             Object& set_color_with_alpha(Color color);
+            /** @brief Set the opacity of this object
+             *
+             * Note that this class doesn't actually keep track of an opacity
+             * value separately from the opacity in the color.
+             *
+             * @param opacity The opacity to change to, from 0 to 1.  0 is fully
+             * transparent, while 1 is fully opaque.
+             */
             Object& set_opacity(double opacity);
+            /** @brief Get the color of this object, including alpha. */
             Color get_color() const {return M_color;}
 
             GANIM_TRANSFORMABLE_CHAIN_DECLS(Object);
@@ -42,6 +57,15 @@ namespace ganim {
     };
 }
 
+/** @brief Defines the @ref ganim::Object "Object" chainable functions in a
+ * subclass to allow chaining in the subclass as well
+ *
+ * To use it, just call this macro in the public part of the class declaration,
+ * where the Type parameter is the name of the subclass.  Note that it will
+ * automatically define the @ref ganim::Transformable "Transformable" chainable
+ * functions as well, so you don't need to use both @ref
+ * GANIM_TRANSFORMABLE_CHAIN_DECLS and this macro.
+ */
 #define GANIM_OBJECT_CHAIN_DECLS(Type) \
     GANIM_TRANSFORMABLE_CHAIN_DECLS(Type) \
     Type& set_color(Color color) \
