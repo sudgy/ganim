6f7fb8b5a26814f7cc4a3e016e31ff15204b9cbe
David Cohoe <sudgylacmoe@gmail.com>
2024-03-06 21:17:07 -0800

    Added support for making a gl::Texture from memory

--- src/ganim/gl/texture.cpp
+++ src/ganim/gl/texture.cpp
@@ -4,6 +4,23 @@
 
 using namespace ganim::gl;
 
+Texture::Texture(std::uint8_t* data, int width, int height)
+{
+    glGenTextures(1, &M_id);
+    glBindTexture(GL_TEXTURE_2D, M_id);
+    glTexParameteri(
+        GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR
+    );
+    glTexParameteri(
+        GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR
+    );
+    glTexImage2D(
+        GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
+        GL_RGBA, GL_UNSIGNED_BYTE, data
+    );
+    glGenerateMipmap(GL_TEXTURE_2D);
+}
+
 Texture::Texture()
 {
     glGenTextures(1, &M_id);
--- src/ganim/gl/texture.hpp
+++ src/ganim/gl/texture.hpp
@@ -5,18 +5,32 @@
  * @brief Contains the @ref ganim::gl::Texture "Texture" class
  */
 
+#include <cstdint>
+
 namespace ganim::gl {
-    /** @brief A thin RAII wrapper around OpenGL textures.
+    /** @brief An RAII wrapper around OpenGL textures.
      *
      * The constructor calls `glGenTextures` while the destructor calls
      * `glDeleteTextures`.  This type is implicitly convertible to an unsigned
      * integer, which is the texture id, so you can pass this type to OpenGL
-     * functions that expect a texture id.
+     * functions that expect a texture id.  It also includes several ways to
+     * construct textures from existing images.
      */
     class Texture {
         public:
             Texture();
             ~Texture();
+            /** @brief Creates a texture from an in-memory image
+             *
+             * @param data The image data.  It should be an array of RGBA pixel
+             * values with `width * height` pixels, so the total size of the
+             * array should be `4 * width * height`.  The first pixel is the
+             * bottom left of the image, and then it advances in the x
+             * direction, then in the y direction.
+             * @param width The width of the image, in pixels
+             * @param height The height of the image, in pixels
+             */
+            Texture(std::uint8_t* data, int width, int height);
             Texture(unsigned id) {M_id = id;}
             Texture(Texture&&) noexcept;
             Texture(const Texture&)=delete;
