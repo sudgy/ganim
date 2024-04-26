5e6dc97eea7374375a2502c58596245b333f7515
David Cohoe <sudgylacmoe@gmail.com>
2024-04-26 10:34:54 -0700

    Fixed some accidental UB

--- src/ganim/animation/creation.cpp
+++ src/ganim/animation/creation.cpp
@@ -9,12 +9,12 @@ void create(Object& object, AnimationArgs args)
     auto anim = Animation(object, args);
     anim.get_starting_object().set_draw_fraction(0);
     anim.get_ending_object().set_draw_fraction(1);
-    object.add_updater(std::move(anim));
-    object.set_visible(true);
-    object.set_creating(true);
     anim.at_end([&object]{
         object.set_creating(false);
     });
+    object.add_updater(std::move(anim));
+    object.set_visible(true);
+    object.set_creating(true);
 }
 
 void noise_create(Object& object, double noise_scale, AnimationArgs args)
@@ -22,12 +22,12 @@ void noise_create(Object& object, double noise_scale, AnimationArgs args)
     auto anim = Animation(object, args);
     anim.get_starting_object().set_draw_fraction(0);
     anim.get_ending_object().set_draw_fraction(1);
-    object.add_updater(std::move(anim));
-    object.set_visible(true);
-    object.set_noise_creating(noise_scale);
     anim.at_end([&object]{
         object.set_noise_creating(0.0);
     });
+    object.add_updater(std::move(anim));
+    object.set_visible(true);
+    object.set_noise_creating(noise_scale);
 }
 
 }
