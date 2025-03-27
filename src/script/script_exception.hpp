070d3391ea33112c5488e46d690959f83974075b
David Cohoe <sudgylacmoe@gmail.com>
2025-03-26 19:03:32 -0700

    Added RuntimeError

--- src/script/script_exception.hpp
+++ src/script/script_exception.hpp
@@ -10,20 +10,45 @@ namespace ganim {
     {
         if (line == -1) {
             return std::format(
-                    "Error at end of file: {}",
+                    "Compile error at end of file: {}",
                     actual_what);
         }
         else {
             return std::format(
-                    "Error on line {} column {}: {}",
+                    "Compile error on line {} column {}: {}",
                     line + 1, column + 1, actual_what);
         }
+    }
+    inline std::string get_runtime_error_message(
+            int line, int column, std::string_view actual_what)
+    {
+        return std::format(
+                "Runtime error on line {} column {}: {}",
+                line + 1, column + 1, actual_what);
     }
     class CompileError : public std::exception {
         public:
             CompileError(int line, int column, std::string_view actual_what)
             {
                 M_what = get_compile_error_message(line, column,actual_what);
+            }
+            virtual const char* what() const noexcept
+            {
+                return M_what.c_str();
+            }
+
+        private:
+            std::string M_what;
+    };
+    class RuntimeError : public std::exception {
+        public:
+            RuntimeError(std::string_view base_what)
+            {
+                M_what = base_what;
+            }
+            RuntimeError(int line, int column, std::string_view actual_what)
+            {
+                M_what = get_runtime_error_message(line, column,actual_what);
             }
             virtual const char* what() const noexcept
             {
