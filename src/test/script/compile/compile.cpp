6961aa63f15a1fd938fa825aed82b135ad38d909
David Cohoe <sudgylacmoe@gmail.com>
2026-03-03 18:05:42 -0800

    Moved some compile tests to new framework

--- src/test/script/compile/compile.cpp
+++ src/test/script/compile/compile.cpp
@@ -29,3 +29,33 @@ test_output(true);
     REQUIRE(get<double>(output[8]) == 5.0);
     REQUIRE(get<std::byte>(output[9]) == std::byte(1));
 }
+
+TEST_CASE("Script error handling", "[script]") {
+    // Just check that an empty script works
+    // Commented out because currently the thing that this is checking lives in
+    // the test code which doesn't need to get tested.  Maybe uncomment this
+    // when the rest of the framework is built?
+    //auto script = run_script("");
+
+    REQUIRE_THROWS(run_script("  foobar"));
+}
+
+TEST_CASE("Comments", "[script]") {
+    auto test = run_script(R"(
+/**/
+var a = 5; // Single line comment
+var b = 6; /* Multi line comment
+b = 7;
+*/ var c = 8;
+test_output(a);
+test_output(b);
+test_output(c);
+)");
+    REQUIRE(test.size() == 3);
+    REQUIRE(get<int64_t>(test[0]) == 5);
+    REQUIRE(get<int64_t>(test[1]) == 6);
+    REQUIRE(get<int64_t>(test[2]) == 8);
+    REQUIRE_THROWS(run_script("/*"));
+    REQUIRE_THROWS(run_script("*/"));
+    REQUIRE_THROWS(run_script("/*/"));
+}
