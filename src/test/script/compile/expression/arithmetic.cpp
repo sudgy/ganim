6712098b04ab06b3bf4e6df72387062e3d5dd21f
David Cohoe <sudgylacmoe@gmail.com>
2026-03-03 17:55:18 -0800

    Moved some tests around to files that fit better

---
+++ src/test/script/compile/expression/arithmetic.cpp
@@ -0,0 +1,29 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "test/script/run_script.hpp"
+
+using namespace ganim;
+
+TEST_CASE("Complicated arithmetic expressions", "[script]") {
+    auto test = run_script(R"(
+var a = 5;
+var b = 3;
+test_output(b + (a + 3) * b + 4 / (5 % 3) - 3);
+    )");
+    REQUIRE(test.size() == 1);
+    REQUIRE(get<int64_t>(test[0]) == 26);
+}
+
+TEST_CASE("Unary plus/minus", "[script]") {
+    auto test = run_script(R"(
+test_output(+5);
+test_output(-3);
+test_output(3 + - 4);
+test_output(3 ++--+4);
+    )");
+    REQUIRE(test.size() == 4);
+    REQUIRE(get<int64_t>(test[0]) == 5);
+    REQUIRE(get<int64_t>(test[1]) == -3);
+    REQUIRE(get<int64_t>(test[2]) == -1);
+    REQUIRE(get<int64_t>(test[3]) == 7);
+}
